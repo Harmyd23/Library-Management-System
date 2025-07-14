6b364bc04c47ddb5a7c4bd6f1174c28a5ef7bcf1
Abdulhamid <abdulhamidyusuf40@gmail.com>
2025-07-14 11:51:16 +0100

    m

--- app/Routes/Auth.py
+++ app/Routes/Auth.py
@@ -6,9 +6,9 @@ from ..Services import auth
 
 Auth=APIRouter(prefix="/auth")
 @Auth.post("/signup",status_code=status.HTTP_201_CREATED)
-async def Signup(request:User,db:Session=Depends(get_db)):
+async def Signup(request:User):
     print("SIGNUP ROUTE HIT")
-    return auth.signup(request,db)
+    return auth.signup(request)
 
 @Auth.post("/login",status_code=status.HTTP_200_OK)
 async def Login(request:Login,db:Session=Depends(get_db)):
